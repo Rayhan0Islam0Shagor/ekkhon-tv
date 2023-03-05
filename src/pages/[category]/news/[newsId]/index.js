0c2b7e7062f5d296fadd09d9a58b6c6ed12fe386
Rayhan0Islam0Shagor <raihanislamshagor2@gmail.com>
2023-03-06 02:35:51 +0600

    update seo

--- src/pages/[category]/news/[newsId]/index.js
+++ src/pages/[category]/news/[newsId]/index.js
@@ -26,6 +26,42 @@ const NewsDetails = ({ news }) => {
         <meta name="googlebot" content="index, follow" />
         <meta name="google" content="nositelinkssearchbox" />
         <meta name="google" content="notranslate" />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ekhon.tv" />
+        <meta property="og:site_name" content="Ekhon TV" />
+        <meta
+          property="og:image"
+          content={`https://backoffice.ekhon.tv/media/imgAll/${contentDetails[0].ImageBgPath}`}
+        />
+        <meta
+          property="og:image:secure_url"
+          content={`https://backoffice.ekhon.tv/media/imgAll/${contentDetails[0].ImageBgPath}`}
+        />
+        <meta property="og:image:alt" content="Ekhon TV" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:locale:alternate" content="bn_BD" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@ekhontv" />
+        <meta name="twitter:creator" content="@ekhontv" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta
+          name="twitter:image"
+          content={`https://backoffice.ekhon.tv/media/imgAll/${contentDetails[0].ImageBgPath}`}
+        />
+        <meta name="twitter:image:alt" content="Ekhon TV" />
+
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <link rel="canonical" href="https://ekhon.tv" />
       </Head>
 
       <motion.div
